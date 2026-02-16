1cb66d0e4917f04d9d5d38688bc81b2470709220
nuttakit_kongkaew <68010334@kmitl.ac.th>
2026-02-16 09:38:15 +0700

    68010334

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,22 @@
-export class Person {}
+export class Person {
+    firstname : string;
+    lastname : string;
+    private age : number ;
+    static COUNTRY: string = "Thailand";
+
+    constructor(){
+        this.firstname = ""
+        this.lastname = ""
+        this.age = 0
+    }
+
+    getFullName(): string {
+        return `${this.firstname} ${this.lastname}`;
+    }
+    setAge(age : number){
+        this.age = age
+    }
+    getAge(){
+        return this.age
+    }
+}
