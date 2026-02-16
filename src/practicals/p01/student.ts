c626d02b9cf4a9a5a7c561f769afd25e4f009879
nuttakit_kongkaew <68010334@kmitl.ac.th>
2026-02-16 09:21:41 +0700

    68010334

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,13 @@
-export class Student {}
+export class Student {
+  firstname: string;
+  lastname: string;
+
+  constructor() {
+    this.firstname = "";
+    this.lastname = "";
+  }
+
+  printName(): string {
+    return `${this.firstname} ${this.lastname}`;
+  }
+}
