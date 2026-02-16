192ac002b2072634e913a722414b2e2a7a424743
nuttakit_kongkaew <68010334@kmitl.ac.th>
2026-02-16 10:00:50 +0700

    68010334

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,4 +2,19 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
+  generation: number;
+
+  constructor(name: string, generation: number, price: number) {
+    super(name, price);
+    this.generation = generation;
+  }
+
+  getProfile(): string {
+    return `${this.name} (Gen ${this.generation})`;
+  }
+
+  getDiscountPrice(): number {
+    const discount = this.price * Product.DISCOUNT_PERCENT / 100;
+    return this.price - discount;
+  }
 }
